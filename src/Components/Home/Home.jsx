f982ee111c4210950ed127436518b64ec1208944
uyiplay44 <uyiplay44@hotmail.com>
2023-07-25 22:58:39 +0000

    Middle

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { About } from '../About/About';
+import { ContentHome } from '../ContentHome/ContentHome';
 
 export const Home = () => {
   return (
     <div>
+      <ContentHome />
       <About />
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.jpg';
+import { RiCloseFill, RiMenu3Fill } from 'react-icons/ri';
+import { useState } from 'react';
+
 
 export const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav)
+  }
   return (
-    <div>
-      navbar
+    <div className='Navbar_main'>
+      <div className='Nav_logo'>
+        <Link><img src={Logo} alt="nav" /></Link>
+      </div>
+      <ul className={nav ? "Nav-list active" : "Nav-list"}>
+        <li><Link to="/" className='link'>Home</Link></li>
+        <li><Link to="/" className='link'>About</Link></li>
+        <li><Link to="/" className='link'>Contact</Link></li>
+      </ul>
+
+      <div className='Menu' onClick={handleClick}>
+        {!nav ? <RiMenu3Fill /> : <RiCloseFill />}
+      </div>
     </div>
   )
 }
