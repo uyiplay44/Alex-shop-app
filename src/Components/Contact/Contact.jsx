2a8a8f25d0948864eefb6453ebaaa7013b794db0
uyiplay44 <uyiplay44@hotmail.com>
2023-08-09 01:36:24 +0000

    contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,12 +3,11 @@ import './Contact.css'
 
 export const Contact = () => {
   return (
-    <div>
-      <div>
-        <div className="map">
-         <h3>Hello world to me world</h3>
-         
-        </div>
+    <div className='ContactMain'>
+      <div className='ContactContainer'>
+        <form>
+          
+        </form>
       </div>
     </div>
   )
